0c69826dddffb8803719a2c570365d031f32d337
TrainedTREX <konarkowski@mac.com>
2024-01-18 15:51:18 -0500

    Update SpotifyAPI.py

--- python-API/SpotifyAPI.py
+++ python-API/SpotifyAPI.py
@@ -46,58 +46,10 @@ class SpotifyAPI:
         return
     
     def offline(self):
-
+        
         #workflow to return json when user is offline.
+        isOffline = bool
 
-        return
-    
-    def propagate(self):
-        
-        return
-    
-    def generateCZML(self, interval):
-        
-        name = "TLE_Propagation"
-        description = "CZML document containing propagated TLE data from all of the satellites contained in the input file."
-        prettyFormatting = True
-        requestedInterval = interval
-        #print(interval)
-        #print(interval.Start)
-        clock = Clock()
-        clock.Interval = interval 
-        clock.CurrentTime = interval.Start
-        
-        CzmlDoc = CzmlDocument()
-        CzmlDoc.Name = name
-        CzmlDoc.Description = description
-        CzmlDoc.PrettyFormatting = prettyFormatting
-        CzmlDoc.RequestedInterval = requestedInterval
-        CzmlDoc.Clock = clock
-    
-        print("Done generating the CZML Document")
-        return CzmlDoc
-    
-    def getOrbitRegime(self,TLE):
-    
-        ecc = TLE.Eccentricity
-        meanMotion = TLE.MeanMotion
-        
-        a = pow((8681663.653 / meanMotion),(2 / 3))
-        
-        hp = a * (1 - ecc) - 6371
-        ha = a * (1 + ecc) - 6371
-        
-        if (ha < 2000):
-            orbitType = 'LEO'
-        elif (hp > (40164 - 6371) and ha < (44164 - 6371)):
-            orbitType = 'GEO'
-        elif (hp > 2000 and ha < (40164 - 6371)):
-            orbitType = 'MEO'
-        elif (hp < 2000 and ha > (40164 - 6371)):
-            orbitType = 'GTO'
-        else:
-            orbitType = 'HEO'
-            
-        return orbitType
+        return isOffline
     
         
